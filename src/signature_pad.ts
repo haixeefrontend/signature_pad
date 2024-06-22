4895aa4d6b1008007606e198fcf9f4ea5a395681
Maiko Tan <maiko.tan.coding@gmail.com>
2024-06-22 10:02:06 +0800

    feat: add crop blank edges feature

--- src/signature_pad.ts
+++ src/signature_pad.ts
@@ -159,6 +159,48 @@ export default class SignaturePad extends SignatureEventTarget {
     });
   }
 
+  public cropSignatureCanvas() {
+    const canvas = this.canvas;
+
+    // First duplicate the canvas to not alter the original
+    const croppedCanvas = document.createElement('canvas'),
+      croppedCtx = croppedCanvas.getContext('2d')!;
+
+    croppedCanvas.width = canvas.width;
+    croppedCanvas.height = canvas.height;
+    croppedCtx.drawImage(canvas, 0, 0);
+
+    // Next do the actual cropping
+    let w = croppedCanvas.width,
+      h = croppedCanvas.height,
+      x: number, y: number, index: number;
+    const pix = { x: [] as number[], y: [] as number[] },
+      imageData = croppedCtx.getImageData(0, 0, croppedCanvas.width, croppedCanvas.height);
+
+    for (y = 0; y < h; y++) {
+      for (x = 0; x < w; x++) {
+        index = (y * w + x) * 4;
+        if (imageData.data[index] < 255 || imageData.data[index + 1] < 255 || imageData.data[index + 2] < 255) {
+          pix.x.push(x);
+          pix.y.push(y);
+        }
+      }
+    }
+    pix.x.sort(function (a, b) { return a - b });
+    pix.y.sort(function (a, b) { return a - b });
+    const n = pix.x.length - 1;
+
+    w = pix.x[n] - pix.x[0];
+    h = pix.y[n] - pix.y[0];
+    const cut = croppedCtx.getImageData(pix.x[0], pix.y[0], w, h);
+
+    croppedCanvas.width = w;
+    croppedCanvas.height = h;
+    croppedCtx.putImageData(cut, 0, 0);
+
+    return croppedCanvas.toDataURL();
+  }
+
   public toDataURL(
     type: 'image/svg+xml',
     encoderOptions?: ToSVGOptions,
